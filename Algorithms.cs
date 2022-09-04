541b8e2163190bee5f5f7ca507e724d3d00a3644
Ivan <PonomarevIK99@yandex.ru>
2022-09-04 14:03:17 +0500

    Fixed null exception when picking edge color

--- Algorithms.cs
+++ Algorithms.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Media3D;
 
 namespace Seven_Bridges
 {
